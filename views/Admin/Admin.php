c31511a493af88afa97cea75b5e527066dfc79bb
itzibtihal <ibtihalboukhanchouch@gmail.com>
2024-01-10 05:20:07 +0100

    update dash view

--- views/Admin/Admin.php
+++ views/Admin/Admin.php
@@ -15,7 +15,7 @@
     <div class="d-flex" id="wrapper">
         <!-- Sidebar -->
         <div class="bg" id="sidebar-wrapper">
-            <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i class="fas fa-house-user me-2"></i>Immo</div>
+            <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i class="fas fa-house-user me-2"></i>Wiki</div>
             <div class="list-group list-group-flush my-3">
                 <a href="Dashboard" class="list-group-item list-group-item-action bg-transparent second-text active"><i class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
 
